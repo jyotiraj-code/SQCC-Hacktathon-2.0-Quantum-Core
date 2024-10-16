0aeb9e8e8202d6b4d38988cd579b4bc4e2c69df1
Jyotiraj Nath <145816462+jyotiraj-code@users.noreply.github.com>
2024-10-16 06:06:31 +0530

    Update main.py

--- backend_theoretical/main.py
+++ backend_theoretical/main.py
@@ -67,42 +67,6 @@ def main(er, budget=None):
 
         return higher_prob_key_reversed, higher_prob_key, best_known_solution, approximation_ratio
 
-        # utils.is_apply_noise = True
-
-        # print(f"Problem: {problem}")
-        # print("Building Circuit...")
-        # circuit = QuantumWalkQAOA(problem, p=p, m=3)
-        # print("Done!")
-        # print("Optimizing Angles...")
-        # angles = utils.find_optimal_angles(circuit, problem)
-        # print("Done!")
-        # print(f"Optimized Angles: {angles}")
-        # probs = utils.get_probs_dict(circuit, problem, angles)
-        # print(f"Probabilities of Bitstrings: {probs}")
-        # ratio = utils.approximation_ratio(problem, probs)
-        # print(f"Approximation Ratio: {ratio}")
-
-        # os.makedirs("plots", exist_ok=True)
-        # folder = os.path.join("plots", f"{p}_{m}_{datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S')}_NOISE")
-        # os.makedirs(folder, exist_ok=True)
-        # fig, ax = plt.subplots()
-        # fig.set_size_inches(18, 10)
-        # hist(ax, probs, folder=f"{folder}")
-
-        # comments = [
-        #     f"Considered {problem}",
-        #     f"QAOA circuit with {p = } and {m = }",
-        #     f"Optimized angles: {angles}",
-        #     f"Resulting Probabilities: {probs}",
-        #     f"Best known solutions: {bks}",
-        #     f"Approximation Ratio: {ratio}",
-        #     f"Higher the probability: {max(probs.values())} at reversed key: {max(probs, key=probs.get)[::-1]}",
-        #     f"Higher the probability: {max(probs.values())} at key: {max(probs, key=probs.get)}"
-        # ]
-
-        # file_name = f"result.txt"
-        # with open(os.path.join(f"{folder}", file_name), "w") as f:
-        #     f.write("\n".join(comments))
 
 
 if __name__ == "__main__":
